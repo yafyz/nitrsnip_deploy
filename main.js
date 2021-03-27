9eec5d35934690cda702c4eccfca68042d4c78e7
fyz <36511274+yafyz@users.noreply.github.com>
2021-03-27 19:27:09 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,48 @@
+const giturl = "https://github.com/yafyz/nitrsnip";
+const fs = require("fs");
+const chp = require("child_process");
+
+var child = null;
+
+function execasync(cmd) {
+    return new Promise((res, rej)=>{
+        chp.exec(cmd, (err, stdout, _)=>{
+            if (err)
+                return rej(err);
+            res(stdout);
+        })
+    });
+}
+
+function fork_child() {
+    child = chp.fork("src/main.js");
+    child.on("exit", ()=>{
+        console.log("Child has exited, restarting");
+        fork_child();
+    });
+}
+
+(async ()=>{
+    if (!fs.existsSync("src")) {
+        console.log("Cloning...");
+        await execasync(`git clone ${giturl} src`);
+        console.log("Installing dependencies");
+        await execasync("cd src && npm install");
+    }
+
+    setInterval(async ()=>{
+        let ret = await execasync("cd src && git pull");
+        if (ret.trim() == "Already up to date.")
+            console.log("Repo is up to date");
+        else {
+            console.log("Pulled new commits")
+            console.log(ret);
+            console.log("Installing dependencies");
+            await execasync("cd src && npm install");
+            console.log("Restarting nitrsnip");
+            child.kill();
+        }
+    }, 60000)
+
+    fork_child();
+})();
